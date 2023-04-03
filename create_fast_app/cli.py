7b2ba0b084c54c4f6dc1650ebfd3994c12270221
chinedu <crankyekeruche@gmail.com>
2023-04-03 14:23:26 -0400

    added verbose messages

--- create_fast_app/cli.py
+++ create_fast_app/cli.py
@@ -13,29 +13,38 @@ from create_fast_app import utils
 @click.argument('project_name')
 def create_fastapi_app(project_name, type, ml):
     """Create a new FastAPI project."""
+    click.echo(f"Creating new {type} project {project_name}...")
     # Get the absolute path of the directory where the script is running
     current_dir = os.getcwd()
     project_dir = os.path.join(current_dir, project_name)
+    
+    click.echo(f"Creating directory {project_dir}...")
     os.makedirs(project_dir)
     
     # Copy the FastAPI project template to the new directory
     if type == "microservice":
+        click.echo("Generating microservice app template...")
         utils.generate_microservice_app(project_dir, attach_ml=ml)
         
     elif type == "monolith":
+        click.echo("Generating monolith app template...")
         utils.generate_monolith_app(project_dir, attach_ml=ml)
         
     elif type == "ml_app":
+        click.echo("Generating machine learning app template...")
         utils.generate_ml_app(project_dir)
         
     else:
-        click.echo("Invalid project given. Must be one of [ml_app, monolith, microservice]")
+        click.echo(f"Invalid project type {type}. Must be one of [ml_app, monolith, microservice]")
         sys.exit(2)
         
+    click.echo("FastAPI project template generated successfully!")
     
     # Create a virtual environment and install the required dependencies
+    click.echo("Creating virtual environment and installing dependencies...")
     os.chdir(project_dir)
     utils.install_packages()
+    click.echo("Dependencies installed successfully!")
     
     # Print the success message
     click.echo(f'Success! Created {project_name} at {current_dir}\{project_name}')
